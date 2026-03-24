9753d88fd5e85f99d713052048dea40e35bdd6c1
Naman Sharma <namansh.devs@gmail.com>
2026-03-24 10:43:39 +0530

    env changes

--- src/components/config/firebase.js
+++ src/components/config/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDhdklkeSBBU6zQBHpbwZLCEqeOHz5QICY",
-  authDomain: "job-portal-34f82.firebaseapp.com",
-  projectId: "job-portal-34f82",
-  storageBucket: "job-portal-34f82.firebasestorage.app",
-  messagingSenderId: "869116184134",
-  appId: "1:869116184134:web:9bf6ff5fc414bb7f252c47"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
